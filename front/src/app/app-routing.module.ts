d1dc0023ee1913024eaca8d00b64c0005c9cbbfe
Chami Rachid <chamirachid1@gmail.com>
2019-11-24 12:03:40 -0800

    Creating groups-list component

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
+import { AppComponent } from './app.component';
+import { GroupsListComponent } from './groups-list/groups-list.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component: AppComponent}, 
+  { path:"groupsComponent", component: GroupsListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
